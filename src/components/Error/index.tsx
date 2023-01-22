7c0cafb41964787b7233ed1792b30df944d25e3a
Medini Hegde <medinihegde305@gmail.com>
2023-01-22 20:55:43 +0530

    font awsome added

--- src/components/Error/index.tsx
+++ src/components/Error/index.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFileCode } from "@fortawesome/free-regular-svg-icons";
 import somethingWrongIcon from "../../assets/icons/png/something-wrong.png";
 
 interface ErrorProps {
@@ -7,12 +9,15 @@ interface ErrorProps {
 
 // TODO: Support for showing actual error message toggle
 function Error({
-  errorMessage = "Ooops!! Something went wrong :(",
+  errorMessage = "Ooops!! Something went wrong",
 }: ErrorProps): JSX.Element {
   return (
     <div className="error-component text-center">
       <img src={somethingWrongIcon} className="image-center" alt="something-wrong" />
-      <div className="message mt-2">{errorMessage}</div>
+      <div className="message mt-2">
+        {errorMessage}
+        <FontAwesomeIcon icon={faFileCode} />
+      </div>
     </div>
   );
 }
